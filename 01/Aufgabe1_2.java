663d6efad18643b51609700c8eb04096b72c56d1
Angie Drucks <angie.drucks@gmail.com>
2025-10-09 12:04:14 +0200

    Implement zip writing and line conversion

--- 01/Aufgabe1_2.java
+++ 01/Aufgabe1_2.java
@@ -1,4 +1,7 @@
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Aufgabe1_2 {
 
@@ -23,9 +26,48 @@ public class Aufgabe1_2 {
             System.exit(1);
         }
 
+        // Block B: Reader und Writer erstellen
 
+        try (
+                // Reader UTF8
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(new FileInputStream(inputFile), StandardCharsets.UTF_8)
+                );
 
+                // Outputstream für die ZIP Datei
+                ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(outputFile));
 
+                // Writer für ISO-8859-1 innerhalb des ZipStreams
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(zipOut, StandardCharsets.ISO_8859_1)
+                )
+        ) {
+            // Block C: Zip Eitrag öffnen und zeilenweise konvertierten
+
+            zipOut.putNextEntry(new ZipEntry(inputFile.getName()));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\r\n"); // LF -> CR+LF
+            }
+
+            writer.flush();
+            zipOut.closeEntry();    // Block D: Zip Eintrag schließen
+
+            // Statusausgabe
+
+            System.out.println("Konvertierung erfolgreich!");
+            System.out.println("Eingabedatei:   " + inputFile.getName() + " (UTF-8, LF)");
+            System.out.println("Ausgabedatei: " + outputFile.getName() + " (ISO-8859-1, CR+LF, ZIP)");
+
+        } catch (IOException e) {
+
+            // Block für Fehlerbehandlung
+            System.err.println("Fehler beim Lesen/Schreiben: " + e.getMessage());
+            System.exit(1);
+        }
 
 
     }
