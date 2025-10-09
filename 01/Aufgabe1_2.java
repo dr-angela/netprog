0eea881080fc087c9dd59fce86f0d91168fa1309
Angie Drucks <angie.drucks@gmail.com>
2025-10-09 11:38:10 +0200

    Add parameter validation for input/output files

--- 01/Aufgabe1_2.java
+++ 01/Aufgabe1_2.java
@@ -1,3 +1,18 @@
 public class Aufgabe1_2 {
 
+    public static void main(String[] args) {
+
+        // Block A: Parameter prüfen (2) und Eingabedatei validieren
+        if (args.length != 2) {
+            System.err.println("Fehler: Es sind genau zwei Parameter erforderlich!");
+            System.err.println("Ausführungsbefehl: java Aufgabe1_2 <Eingabedatei> <Ausgabedatei>");
+            System.err.println("Beispiel: java Aufgabe1_2 utf8files/file1.txt file1_converted.zip")
+            System.exit(1);
+        }
+
+
+
+
+    }
+
 }
