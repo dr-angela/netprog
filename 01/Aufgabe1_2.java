ab42f779c788c884d107652c71b865586ae4393c
Angie Drucks <angie.drucks@gmail.com>
2025-10-09 11:44:50 +0200

    Implement basic parameter check and input file validation

--- 01/Aufgabe1_2.java
+++ 01/Aufgabe1_2.java
@@ -6,9 +6,22 @@ public class Aufgabe1_2 {
         if (args.length != 2) {
             System.err.println("Fehler: Es sind genau zwei Parameter erforderlich!");
             System.err.println("Ausführungsbefehl: java Aufgabe1_2 <Eingabedatei> <Ausgabedatei>");
-            System.err.println("Beispiel: java Aufgabe1_2 utf8files/file1.txt file1_converted.zip")
+            System.err.println("Beispiel: java Aufgabe1_2 utf8files/file1.txt file1_converted.zip");
             System.exit(1);
         }
+
+        // Eingabe und Ausgabepfade instanzieren
+        File inputFile = new File(args[0]);
+        File outputFile = new File(args[1]);
+
+        // Error Handling
+
+        if (!inputFile.exists() || !inputFile.isFile() || !inputFile.canRead()) {
+            System.err.println("Fehler: Eingabedatei ungültig!");
+            System.exit(1);
+        }
+
+
 
 
 
