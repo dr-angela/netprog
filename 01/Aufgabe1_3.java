bbc6e93efa0d5e21230d75c19592d501c5b8b62f
Angie Drucks <angie.drucks@gmail.com>
2025-10-09 15:53:17 +0200

    started with Aufgabe 1.3 - byte [] mit isData und isUrgent

---
+++ 01/Aufgabe1_3.java
@@ -0,0 +1,15 @@
+public class Aufgabe1_3 {
+
+    /**
+     * Erstellt eine Nachricht im angegebenen Transportprotokoll.
+     *
+     * @param isData         D-Flag: true = Datennachricht
+     * @param isUrgent       U-Flag: true = dringende Nachricht
+     * @param sequenceNumber Sequenznummer (0-65535)
+     * @param payload        Nutzdaten (byte array)
+     * @return Byte[] mit Nachricht
+     * @throws IllegalArgumentException wenn param falsch
+     */
+
+
+}
